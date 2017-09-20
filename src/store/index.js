87cf31be40aac6c769f9a173ea8be8309cd04c1b
Edmar Diaz <diaz.edmar@outlook.com>
2017-09-20 20:37:37 +0800

    20/09/17
     -Added Create meetups

---
+++ src/store/index.js
@@ -0,0 +1,63 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+  state: {
+    loadedMeetups: [
+      {
+        imageUrl: 'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
+        id: '1',
+        title: 'Meetup in NY',
+        date: '2017-09-20'
+      },
+      {
+        imageUrl: 'https://media.timeout.com/images/103531855/image.jpg',
+        id: '2',
+        title: 'Meetup in Paris',
+        date: '2017-09-22'
+      }
+    ],
+    user: {
+      id: '32dsdsa',
+      registeredMeetups: ['safdsf2323']
+    }
+  },
+  mutations: {
+    createMeetup (state, payload) {
+      state.loadedMeetups.push(payload)
+    }
+  },
+  actions: {
+    createMeetup ({commit}, payload) {
+      const meetup = {
+        title: payload.title,
+        location: payload.location,
+        imageUrl: payload.imageUrl,
+        description: payload.description,
+        date: payload.date,
+        id: 'sadas123'
+      }
+      // firebase and store it
+      commit('createMeetup', meetup)
+    }
+  },
+  getters: {
+    loadedMeetups (state) {
+      return state.loadedMeetups.sort((meetupA, meetupB) => {
+        return meetupA.date > meetupB.date
+      })
+    },
+    featuredMeetups (state, getters) {
+      return getters.loadedMeetups.slice(0, 5)
+    },
+    loadedMeetup (state) {
+      return (meetupId) => {
+        return state.loadedMeetups.find((meetup) => {
+          return meetup.id === meetupId
+        })
+      }
+    }
+  }
+})
